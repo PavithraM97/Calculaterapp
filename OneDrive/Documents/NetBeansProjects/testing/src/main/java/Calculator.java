058192347507f90ebc0605a96ff22a1fb58193ee
Your NamePavithraM97 <you@example.com>
2023-03-04 04:33:20 -1200

    webapp

---
+++ OneDrive/Documents/NetBeansProjects/testing/src/main/java/Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pmadu
+ */
+public class Calculator {
+    public int add (int no1 , int no2){
+        int sum = no1+no2;
+        return sum;
+    }
+}
